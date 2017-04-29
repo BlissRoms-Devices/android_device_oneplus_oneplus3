b149daf6f30535c3985b9a0f6ed66556d7312a39
acuicultor <ramoncuenca4@gmail.com>
2017-04-29 20:02:06 +0200

    Dont use sdclang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,9 +33,6 @@ BOARD_VENDOR := oneplus
 # Assertions
 TARGET_OTA_ASSERT_DEVICE := OnePlus3,oneplus3,OnePlus3T,oneplus3t
 
-# Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := true
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8996
 TARGET_NO_BOOTLOADER := true
